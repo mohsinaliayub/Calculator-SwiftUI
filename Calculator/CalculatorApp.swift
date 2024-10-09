61269b0ada2e8381acc5a779e70da92458f67f5e
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-10-09 10:15:30 +0200

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Mohsin Ali Ayub on 09.10.24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
