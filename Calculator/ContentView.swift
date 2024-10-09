63dd0e74e050a403ebfb6a57bbd0bd2a65f45f24
Mohsin Ali Ayub <mosin.ayub@outlook.com>
2024-10-09 13:07:03 +0200

    bare basic UI

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -8,14 +8,97 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack(alignment: .trailing, spacing: 20) {
+                HStack {
+                    Spacer()
+                    Text("OUTPUT")
+                        .font(.largeTitle)
+                }
+                HStack {
+                    Text("RESULT")
+                        .lineLimit(1)
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            
+            Spacer()
+            HStack {
+                NumberButton(number: 7)
+                NumberButton(number: 8)
+                NumberButton(number: 9)
+                ActionButton(action: .multiply)
+            }
+            HStack {
+                NumberButton(number: 4)
+                NumberButton(number: 5)
+                NumberButton(number: 6)
+                ActionButton(action: .subtract)
+            }
+            HStack {
+                NumberButton(number: 1)
+                NumberButton(number: 2)
+                NumberButton(number: 3)
+                ActionButton(action: .add)
+            }
+            HStack {
+                ActionButton(action: .sign)
+                NumberButton(number: 0)
+                ActionButton(action: .clear)
+                ActionButton(action: .equal)
+            }
         }
-        .padding()
+    }
+}
+
+struct NumberButton: View {
+    let number: Int
+    
+    var body: some View {
+        Button {
+            
+        } label: {
+            Text("\(number)")
+        }
+        .foregroundStyle(.black.opacity(0.8))
+        .font(.largeTitle)
+        .frame(width: 80, height: 80)
+        .background(Circle().fill(.gray.opacity(0.1)))
+    }
+}
+
+struct ActionButton: View {
+    let action: Action
+    
+    var body: some View {
+        Button {
+            
+        } label: {
+            Image(systemName: action.systemIcon)
+        }
+        .foregroundStyle(.green)
+        .font(.largeTitle)
+        .frame(width: 80, height: 80)
+        .background(Circle().fill(.gray.opacity(0.1)))
+    }
+}
+
+enum Action: String {
+    case divide
+    case multiply
+    case subtract = "minus"
+    case add = "plus"
+    case equal
+    case sign = "plus.forwardslash.minus"
+    case clear = "C"
+    
+    var systemIcon: String {
+        rawValue
     }
 }
 
